de9d248f1f0fb90ab85a54f8fc3a369681ca808b
satish chandra <imsachandra@gmail.com>
2025-01-31 02:53:20 +0530

    1st commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,73 @@
-'use strict';
+// Light and dark mode
+// const themeToggle = document.getElementById("themeToggle");
+// const root = document.documentElement;
 
+// themeToggle.addEventListener("click", () => {
+//   if (root.style.getPropertyValue(" --bg-light: hsla(0, 0%, 100%, 1)")) {
+//     // Switch to dark mode
+//     root.style.setProperty("--bg-dark: hsla(0, 0%, 0%, 1)");
+//     root.style.setProperty(" --text-light: hsla(0, 0%, 0%, 1)");
+//     // themeToggle.textContent = ""; // Change to Sun icon
+//   } else {
+//     // Switch to light mode
+//     root.style.setProperty(" --bg-light: hsla(0, 0%, 100%, 1)");
+//     root.style.setProperty("--text-dark: hsla(0, 0%, 100%, 1)");
+//     themeToggle.textContent = ""; // Change to Moon icon
+//   }
+// });
+// const themeToggle = document.getElementById("themeToggle");
+// const root = document.documentElement;
 
+// themeToggle.addEventListener("click", () => {
+//   if (body.classList.contains("dark-mode")) {
+//     body.classList.remove("dark-mode");
+//     body.classList.add("light-mode");
+//       themeToggle.innerHTML = `<i class="fa-solid fa-moon"></i>`; // Moon icon
+//   } else {
+//     body.classList.remove("light-mode");
+//     body.classList.add("dark-mode");
+//      themeToggle.innerHTML = `<i class="fa-solid fa-sun"></i>`; // Sun icon
+//   }
+// });
+
+// const themeToggle = document.getElementById("themeToggle");
+// const root = document.documentElement;
+
+// // Check for saved theme in localStorage
+// if (localStorage.getItem("theme") === "dark") {
+//   enableDarkMode();
+// } else {
+//   enableLightMode();
+// }
+
+// // Theme Toggle Button Click Event
+// themeToggle.addEventListener("click", () => {
+//   if (localStorage.getItem("theme") === "dark") {
+//     enableLightMode();
+//   } else {
+//     enableDarkMode();
+//   }
+// });
+
+//-------------------------------------------------------------------------------------------->
+
+function enableDarkMode() {
+  root.style.setProperty("--bg-color", "var(--bg-black)");
+  root.style.setProperty("--text-color", "var(--text-white)");
+  root.style.setProperty("--gradient-bg", "var(--gradient-1)");
+  
+  localStorage.setItem("theme", "dark");
+ 
+}
+
+function enableLightMode() {
+  root.style.setProperty("--bg-color", "var(--bg-light)");
+  root.style.setProperty("--text-color", "var(--text-black)");
+  root.style.setProperty("--gradient-bg", "var(--gradient-2)");
+  
+  localStorage.setItem("theme", "light");
+
+}
 
 /**
  * PRELOADER
@@ -13,8 +80,6 @@ window.addEventListener("DOMContentLoaded", function () {
   document.body.classList.add("loaded");
 });
 
-
-
 /**
  * add event on multiple elements
  */
@@ -23,9 +88,7 @@ const addEventOnElements = function (elements, eventType, callback) {
   for (let i = 0, len = elements.length; i < len; i++) {
     elements[i].addEventListener(eventType, callback);
   }
-}
-
-
+};
 
 /**
  * Mobile navbar toggle
@@ -48,8 +111,6 @@ addEventOnElements(navLinks, "click", function () {
   document.body.classList.remove("nav-active");
 });
 
-
-
 /**
  * Header active
  */
@@ -60,8 +121,6 @@ window.addEventListener("scroll", function () {
   header.classList[window.scrollY > 100 ? "add" : "remove"]("active");
 });
 
-
-
 /**
  * Element tilt effect
  */
@@ -69,7 +128,6 @@ window.addEventListener("scroll", function () {
 const tiltElements = document.querySelectorAll("[data-tilt]");
 
 const initTilt = function (event) {
-
   /** get tilt element center position */
   const centerX = this.offsetWidth / 2;
   const centerY = this.offsetHeight / 2;
@@ -77,17 +135,16 @@ const initTilt = function (event) {
   const tiltPosY = ((event.offsetX - centerX) / centerX) * 10;
   const tiltPosX = ((event.offsetY - centerY) / centerY) * 10;
 
-  this.style.transform = `perspective(1000px) rotateX(${tiltPosX}deg) rotateY(${tiltPosY - (tiltPosY * 2)}deg)`;
-
-}
+  this.style.transform = `perspective(1000px) rotateX(${tiltPosX}deg) rotateY(${
+    tiltPosY - tiltPosY * 2
+  }deg)`;
+};
 
 addEventOnElements(tiltElements, "mousemove", initTilt);
 
 addEventOnElements(tiltElements, "mouseout", function () {
   this.style.transform = `perspective(1000px) rotateX(0deg) rotateY(0deg)`;
 });
-
-
 
 /**
  * Tab content
@@ -100,37 +157,35 @@ let lastActiveTabBtn = tabBtns[0];
 let lastActiveTabContent = tabContents[0];
 
 const filterContent = function () {
-
   if (!(lastActiveTabBtn === this)) {
-
     lastActiveTabBtn.classList.remove("active");
     lastActiveTabContent.classList.remove("active");
 
     this.classList.add("active");
     lastActiveTabBtn = this;
 
-    const currentTabContent = document.querySelector(`[data-tab-content="${this.dataset.tabBtn}"]`);
+    const currentTabContent = document.querySelector(
+      `[data-tab-content="${this.dataset.tabBtn}"]`
+    );
 
     currentTabContent.classList.add("active");
     lastActiveTabContent = currentTabContent;
-
   }
-
-}
+};
 
 addEventOnElements(tabBtns, "click", filterContent);
-
-
 
 /**
  * Custom cursor
  */
 
 const cursors = document.querySelectorAll("[data-cursor]");
-const hoveredElements = [...document.querySelectorAll("button"), ...document.querySelectorAll("a")];
+const hoveredElements = [
+  ...document.querySelectorAll("button"),
+  ...document.querySelectorAll("a"),
+];
 
 window.addEventListener("mousemove", function (event) {
-
   const posX = event.clientX;
   const posY = event.clientY;
 
@@ -143,7 +198,6 @@ window.addEventListener("mousemove", function (event) {
     cursors[1].style.left = `${posX}px`;
     cursors[1].style.top = `${posY}px`;
   }, 80);
-
 });
 
 /** add hovered class when mouseover on hoverElements */
